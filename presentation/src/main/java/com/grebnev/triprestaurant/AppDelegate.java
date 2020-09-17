c35ddfa0b941080a2e77250ed7ef2b35a58868b4
grebnev <grbnev.developer@gmail.com>
2020-09-17 17:47:44 +0400

    Add moxy and cicirone

--- presentation/src/main/java/com/grebnev/triprestaurant/AppDelegate.java
+++ presentation/src/main/java/com/grebnev/triprestaurant/AppDelegate.java
@@ -5,14 +5,22 @@ import android.app.Application;
 import com.grebnev.triprestaurant.di.AppComponent;
 import com.grebnev.triprestaurant.di.DaggerAppComponent;
 
+import ru.terrakok.cicerone.Cicerone;
+import ru.terrakok.cicerone.Router;
+
 public class AppDelegate extends Application {
 
     private static AppComponent appComponent;
+    private Cicerone<Router> cicerone;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
         appComponent = DaggerAppComponent.create();
+
+        cicerone = Cicerone.create();
     }
+
+
 }
---
+++ presentation/src/main/java/com/grebnev/triprestaurant/common/SingleFragmentActivity.java
@@ -0,0 +1,6 @@
+package com.grebnev.triprestaurant.common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SingleFragmentActivity extends AppCompatActivity {
+}
